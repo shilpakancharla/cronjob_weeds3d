643be4ea112a487191ef72faa1f34831bf1586e5
Shilpa Kancharla <shilpakancharla18@gmail.com>
2021-06-08 16:51:44 -0400

    Split script into two parts

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### cron Job I
 
-Check the Azure Storage container for recent uploads at the specifie date. Write the name of the file unprocessed file to the text file.
+Check the Azure Storage container for recent uploads at the specifie date. Write the name of the file unprocessed file to the text file. Specifically, run the `capture_blob.py` script.
 
 ### cron Job II
 
-Check the text file to grab the name of the blob. Run this blob through the process. Once processed, delete the name from the file.
+Check the text file to grab the name of the blob. Run this blob through the process. Once processed, delete the name from the file. Specifically, run the `event_process.py` script.
--- event_process.py
+++ event_process.py
@@ -1,75 +1,14 @@
-import pytz
-import keys as key
-from datetime import datetime, timedelta
-from azure.storage.blob import BlobServiceClient, BlobSasPermissions, generate_blob_sas
-
-"""
-    Access contents of the Azure blob storage. Write the name of the blob uploaded to the text file.
-    Retrieve the most recently uploaded block blob URL.
-
-    @param account_name: name of the Azure Storage account (can change this parameter in the driver code)
-    @param container_name: string name of container (can change this parameter in the driver code)
-    @return dictionary that has name of the blob as a key, and SAS URL of the blob as the value
-"""
-def retrieve_blob(account_name, container_name):
-    # Instantiate a BlobServiceClient using a connection string
-    blob_service_client = BlobServiceClient.from_connection_string(key.CONNECTION_STRING_1)
-    container_client = blob_service_client.get_container_client(container_name)
-
-    # Collect all the blob names that are past June 1, 2021
-    utc = pytz.UTC
-    last_date = utc.localize(datetime(2021, 6, 1))
-    blob_name_date_dictionary = dict()
-    for blob in container_client.list_blobs():
-        if blob.last_modified > last_date:
-            blob_name_date_dictionary[blob.name] = generate_sas(account_name, container_name, blob.name)
-    
-    # Check to see if blob file has already been processed by checking the processed file list text
-    with open ('processed_blobs.txt', 'r+') as file:
-        for blob_name in blob_name_date_dictionary.keys():
-            for line in file:
-                if blob_name in line:
-                    try:
-                        # Remove element from dictionary because it has already been processed
-                        del blob_name_date_dictionary[blob_name]
-                    except KeyError:
-                        print(blob_name + " was not in the list of blobs to be processed.")
-                        pass
-    
-    # Write the name of the blob file to blob_list.txt
-    with open('blob_list.txt', 'r+') as file:
-        for blob_name in blob_name_date_dictionary.keys():
-            for line in file:
-                if blob_name in line: # If the name of the blob already exists in the file, do not write it
-                    break
-            else:
-                file.write(blob_name)
-                file.write("\n")
-    
-    return blob_name_date_dictionary
-
-"""
-    Generates the SAS token that needs to be at the end of a blob storage URL for download.
-
-    @param account_name: name of the Azure Storage account
-    @param container_name: string name of container
-    @param blob_name: name of blob that needs to be downloaded
-    @return URL of the most recently uploaded blob to the storage container with the SAS token at the end
-"""
-def generate_sas(account_name, container_name, blob_name):
-    sas = generate_blob_sas(account_name = account_name, 
-                    account_key = key.KEY_1, 
-                    container_name = container_name,
-                    blob_name = blob_name, 
-                    permission = BlobSasPermissions(read = True),
-                    expiry = datetime.utcnow() + timedelta(hours = 2))
-    sas_url = 'https://' + account_name + '.blob.core.usgovcloudapi.net/' + container_name + '/' + blob_name + '?' + sas
-    return sas_url
+import subprocess
+import pandas as pd 
 
 """
     Run the clustering and analysis process on the blob.
+
+    @param blob_name: name of the blob file that needs to removed from unprocessed blob list to processed file list
 """
-def run_process():
+def run_process(blob_name, src, dest):
+    # Copy the blob into the appropriate folder
+
     return
 
 """
@@ -82,6 +21,18 @@ def delete_blob_from_list(blob_name):
     return
 
 if __name__ == "__main__":
-    storage_account = key.STORAGE_ACCOUNT
-    container_name = key.CONTAINER_NAME
-    blob_name_date_dictionary = retrieve_blob(storage_account, container_name)
+    # Get the blob at the top of the blob_list.txt
+    first_line = ""
+    with open("blob_list.txt", "r") as file:
+        first_line = file.readline()
+
+    first_line = first_line.replace("\n", "")
+    # Convert blobs.csv to dataframe
+    blob_df = pd.read_csv('blobs.csv', sep = ',', names = ['Name', 'URL'])
+    blob_df = blob_df.astype({'Name': "str"})
+    blob_df = blob_df.astype({'URL': "str"})
+
+    # Get the URL that corresponds to the name of the blob
+    src = blob_df[blob_df['Name'] == first_line]['URL'].values[0]
+
+    run_process(first_line, src, dest)
